733f8c93a477ea994be830632d1bcbef10b484fa
pulin <purinza2006@gmail.com>
2026-07-29 17:25:25 +0700

    rm middleware comment kub

--- src/routes/enrollmentRouters_v2.ts
+++ src/routes/enrollmentRouters_v2.ts
@@ -13,8 +13,6 @@ import { users, students, courses } from "../db/db.js";
 // import middleware
 import { authenticateToken } from "../middlewares/authenMiddleware.ts";
 import type { CustomRequest } from "../libs/types.ts";
-// import { checkRoleAdmin } from "../middlewares/checkRoleAdminMiddleware.ts";
-// import { checkRoles } from "../middlewares/checkRolesMiddleware.ts";
 
 const router = Router();
 
